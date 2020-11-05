41a9d90fe85b93d058e57c83b36f0bf8940d190a
EC2 Default User <ec2-user@ip-172-31-79-54.ec2.internal>
2020-11-05 10:16:30 +0000

    [update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,20 @@ class User < ApplicationRecord
   end
 
 
+  def self.search(search,word)
+            if search == "forward_match"
+                           @user = User.where("name LIKE?","#{word}%")
+            elsif search == "backward_match"
+                           @user = User.where("name LIKE?","%#{word}")
+            elsif search == "perfect_match"
+                           @user = User.where("#{word}")
+            elsif search == "partial_match"
+                           @user = User.where("name LIKE?","%#{word}%")
+            else
+                           @user = User.all
+            end
+  end
+
   attachment :profile_image
 
   validates :name, presence: true, length:{ minimum: 2, maximum: 20 }
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
 
   post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+
+  get 'search' => 'searchs#search'
 end
