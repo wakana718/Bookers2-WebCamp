22bd3e1d3cd66b48b7dc638dae079ba9eea2b3e7
EC2 Default User <ec2-user@ip-172-31-79-54.ec2.internal>
2020-11-09 18:35:30 +0000

    [update]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,15 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :name,
+      :email,
+      :postcode,
+      :prefecture_name,
+      :address_city,
+      :address_street,
+      :address_building
+      ])
   end
 
   def after_sign_in_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -48,4 +48,17 @@ class User < ApplicationRecord
   validates :name, presence: true, length:{ minimum: 2, maximum: 20 }
   validates :introduction, length:{ maximum: 50 }
 
+  # 住所検索
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  def prefecture_name
+   JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+   self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
 end
+
